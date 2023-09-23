45892e16e651c77cad38cc001357fc55a6261dd4
Adrian Burgos <awb8593@rit.edu>
2023-09-23 15:53:38 -0400

    fixed decorator not valid error

--- voyage/src/app/plan-for-me/plan-for-me.component.ts
+++ voyage/src/app/plan-for-me/plan-for-me.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { apiKey } from './api-key';
 import OpenAI from 'openai';
 
+const openai = new OpenAI({
+  apiKey: apiKey
+})
+
+
+
 @Component({
   selector: 'app-plan-for-me',
   templateUrl: './plan-for-me.component.html',
   styleUrls: ['./plan-for-me.component.css']
-})
-
-const openai = new OpenAI({
-  apiKey: apiKey
 })
 
 export class PlanForMeComponent {
