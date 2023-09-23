09435f705e9a23fc5c342da0522f41fb4098206a
Adrian Burgos <awb8593@rit.edu>
2023-09-23 15:46:41 -0400

    imported OpenAI

--- voyage/src/app/plan-for-me/plan-for-me.component.ts
+++ voyage/src/app/plan-for-me/plan-for-me.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { apiKey } from './api-key';
+import OpenAI from 'openai';
 
 @Component({
   selector: 'app-plan-for-me',
   templateUrl: './plan-for-me.component.html',
   styleUrls: ['./plan-for-me.component.css']
 })
-export class PlanForMeComponent {
 
+const openai = new OpenAI({
+  apiKey: apiKey
+})
+
+export class PlanForMeComponent {
+  
 }
