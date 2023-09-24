d957a3455330c046d10224aef2e01d1460a37c6e
Zoe <zmb6893@g.rit.edu>
2023-09-24 03:21:29 -0400

    Added routing to trip form

--- voyage/src/app/app-routing.module.ts
+++ voyage/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { ActivityFormComponent } from './activity-form/activity-form.component';
 import { ManualComponent } from './manual/manual.component';
 
 const routes: Routes = [
-  { path: 'activity-form', component: ActivityFormComponent }
+  { path: 'activity-form', component: ActivityFormComponent },
+  { path: 'trip-form', component: ManualComponent }
 ];
 
 @NgModule({
--- voyage/src/app/app.component.ts
+++ voyage/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'voyage';
+  title = 'voyage'; 
+  constructor(private router: Router) { }
+
+  createTrip() {
+    this.router.navigate(['/trip-form']);
+  }
 }
