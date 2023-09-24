360cffd5020310427186efb99a7c0bc1f96dd1a2
Zoe <zmb6893@g.rit.edu>
2023-09-24 03:38:43 -0400

    Redid routing to start and home and move to other pages

--- voyage/src/app/app-routing.module.ts
+++ voyage/src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ActivityFormComponent } from './activity-form/activity-form.component';
 import { ManualComponent } from './manual/manual.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'activity-form', component: ActivityFormComponent },
   { path: 'trip-form', component: ManualComponent }
 ];
